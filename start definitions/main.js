f0336566158ee66393229da0233fb032f35e747d
DESKTOP-VBHNIK2\wery <netlifyt@gmail.com>
2022-05-11 09:47:05 +0300

    change main

--- start definitions/main.js
+++ start definitions/main.js
@@ -1,108 +1,81 @@
-let numbers = [3,4,0,5,2,1,-5,-50, 173, 13, 35, 29, -130, 1999999, -200000, 12371, 554, -332, -1902, 100099, 88559, -936578]
-for (let index = 0; index < 10000; index++) {
-    numbers.push(Math.floor(Math.random() * 10009999))
+'use strict'
+const Game = new Phaser.Game(800, 800, Phaser.AUTO, 'game-canvas', { preload, create, update })
+let ball;
+let mouse;
+let brick, bricks;
+function preload() {
+Game.load.image('mouse', 'images/platform.png');
+Game.load.image('ball', 'images/ball.png');
+Game.load.image('brick','images/brick.png')
+ 
+let a = [];
+let person = {name: 'Ivan', age: 23, height: 180};
+ 
+}
+ 
+function create() {
+    bricks = Game.add.group();
+    bricks.enableBody = true;
+ 
+    let i
+    for(i=0; i<35; i=i+1){
+        let yCoordinates = parseInt(i%5)
+        let xCoordinates = parseInt(i%7)
+
+        brick = Game.add.sprite(170 + xCoordinates*75, 100 + yCoordinates*40, 'brick');
+        bricks.add(brick);
+
+        brick.body.immovable = true;
+
+        brick.scale.setTo(0.25);
+        brick.anchor.setTo(0.5);
+    }
+ 
+ 
+    mouse = Game.add.sprite(400, 600, 'mouse');
+    Game.physics.enable(mouse, Phaser.Physics.ARCADE);
+    mouse.scale.setTo(0.5)
+    mouse.anchor.setTo(0.5)
+    mouse.body.collideWorldBounds = true;
+    mouse.body.immovable = true;
+ 
+ 
+    //Game.physics.startSystem(Phaser.Physics.ARCADE);
+ 
+ 
+    ball = Game.add.sprite(400, 300, 'ball');
+ 
+    Game.physics.enable(ball, Phaser.Physics.ARCADE);
+ 
+    //  This gets it moving
+    ball.body.velocity.setTo(400, 500);
     
-}
-let numbers2 = [0,-5,-50, 173]
-let numbers3 = [13, 3.5, 29, -130]
-
-function getMin(numbers){
-    let a, minNumber = numbers[0];
-    a=0;
-    numbers[a]
-    for (let a = 0; a < numbers.length; a++) {
-        if(minNumber > numbers[a]){
-            minNumber = numbers[a]
+    ball.body.collideWorldBounds = true;
+    ball.body.bounce.set(1);
+    ball.scale.setTo(0.02, 0.02)
+ 
+ }
+ 
+function update() {
+    Game.physics.arcade.collide(mouse, ball)
+    Game.physics.arcade.collide(ball, bricks, onHitBricks)
+ 
+ 
+    if (Game.input.mousePointer.isUp)
+    {
+        Game.physics.arcade.moveToXY(mouse, Game.input.x, 600, 500, 0);
+        if (Game.input.x > mouse.x - mouse.width/2 && Game.input.x < mouse.x + mouse.width/2)
+        {
+            mouse.body.velocity.setTo(0, 0);
         }
     }
-
-    return minNumber;
-}
-
-function getMinNumberPosition(numbers){
-    let minNumber = getMin(numbers)
-
-    for (let i = 0; i < numbers.length; i++) {
-        if(minNumber == numbers[i]){
-            minNuberPosition = i;
-        }
+    else
+    {
+        mouse.body.velocity.setTo(0, 0);
     }
-
-    return minNuberPosition;
+ 
 }
-
-function sort(numbers){
-    let newNumbers = [];
-
-
-    while(numbers.length > 0){
-        let minNumber = getMin(numbers)
-        let minNuberPosition = getMinNumberPosition(numbers);
-        newNumbers[newNumbers.length] = minNumber;
-        numbers.splice(minNuberPosition, 1);
-    }
-
-    return newNumbers;
+function onHitBricks(ball, br){
+  br.kill()
+  debugger
 }
-
-
-function insertionSort(array){
-    let sortedArray = []
-    debugger;
-    while (array.length > 0) {
-        let newElement = array.pop();
-        sortedArray = addInCorrectPosition(sortedArray, newElement)
-        debugger;
-    }
-    return sortedArray;
-}
-
-function addInCorrectPosition(sortedArray, newElement){
-    if(sortedArray.length === 0){
-        sortedArray.push(newElement)
-    }else{
-        debugger
-        for (let index = 0; index < sortedArray.length; index++) {
-            if(newElement < sortedArray[index]){
-                sortedArray.splice(index, 0, newElement);
-                return sortedArray;
-            }
-        }
-        sortedArray.push(newElement);
-        return sortedArray;
-    }
-
-
-
-    return sortedArray;
-}
-
-// let sortedNumbers = insertionSort(numbers)
-// let sortedNumbers2 = insertionSort(numbers2)
-// let sortedNumbers3 = insertionSort(numbers3)
-
-// console.log(sortedNumbers)
-// console.log(sortedNumbers2)
-// console.log(sortedNumbers3)
-
-
-function measureInsSort() {
-	const start = Date.now();
-	for (let index = 0; index < 10000; index++) {
-        insertionSort(numbers)
-    }
-	const duration = Date.now() - start;
-	return duration;
-}
-
-function measureSort() {
-	const start = Date.now();
-	for (let index = 0; index < 10000; index++) {
-        sort(numbers)
-    }
-	const duration = Date.now() - start;
-	return duration;
-}
-
-console.log(measureInsSort())
-console.log(measureSort())
